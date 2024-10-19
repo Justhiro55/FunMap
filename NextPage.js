412068e46b63b33496ddb51f3e8f66b368319b91
ddde8 <105919676+ddde8@users.noreply.github.com>
2024-10-20 00:15:13 +0900

    Add files via upload

---
+++ NextPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const NextPage = () => {
+  return (
+    <div>
+      <h1>다음 화면입니다!</h1>
+      <p>여기에서 추가 내용을 넣을 수 있습니다.</p>
+    </div>
+  );
+};
+
+export default NextPage;
