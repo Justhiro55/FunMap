b6a57431d2a522815759b1b48db0733429da4380
JustHiro55 <hagi.hiro9829@gmail.com>
2024-10-19 22:51:15 +0900

    feat: Add start and end parameters to GET request for route search

--- backend/handlers/navigation.go
+++ backend/handlers/navigation.go
@@ -1,6 +1,8 @@
 package handlers
 
 import (
+	"fmt"
+	"strconv"
 	"encoding/json"
 	"net/http"
 
@@ -9,6 +11,15 @@ import (
 
 func Navigate(w http.ResponseWriter, r *http.Request) {
 	path := models.GetNavigationPath()
+
+	// Get start and end parameters
+	startStr := r.URL.Query().Get("start")
+	endStr := r.URL.Query().Get("end")
+
+	start, _ := strconv.Atoi(startStr)
+	end, _ := strconv.Atoi(endStr)
+
+	fmt.Println("Start: ", start, "End: ", end)
 
 	response := models.NavigationResponse{Path: path}
 	w.Header().Set("Content-Type", "application/json")
